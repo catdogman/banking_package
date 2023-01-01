a759aa93371a7222dfdb0f88cd8888d7c30509ad
catdogman <51102624+catdogman@users.noreply.github.com>
2022-12-31 20:58:26 -0500

    version 0.0.2 -> version 0.0.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setuptools.setup(
     name="com",
-    version="0.0.2",
+    version="0.0.3",
     author="Thareq",
     author_email="realryangames@gmail.com",
     description= "com library for bca banking",
